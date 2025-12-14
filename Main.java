c44686ede483a00d9b84b5c70ce95348f2a546ca
Dxiswl <noza63146@gmail.com>
2025-12-15 04:45:23 +0700

    Initial commit: добавлены файлы игры

---
+++ Main.java
@@ -0,0 +1,32 @@
+class Main {
+  public static void main(String[] args) {
+    boolean isGreenLight = false;
+
+    int speedOfPlayer1 = 5;
+    int speedOfPlayer2 = 0;
+    int speedOfPlayer3 = 2;
+
+    // Допишите здесь логику так, что будет подсчитано и выведено количество
+    // игроков, которые выбывают. (с 13 строки ответ)
+    // Если свет зелёный, то проходят все игроки (0 выбывают).
+    // Если свет красный, то выбывает каждый, чья скорость не 0.
+
+    int countOfEliminatedPlayers = 0;
+
+    if (isGreenLight == true) {
+      countOfEliminatedPlayers = 0;
+    } else {
+      if (speedOfPlayer1 != 0) {
+        countOfEliminatedPlayers = countOfEliminatedPlayers + 1;
+      }
+      if (speedOfPlayer2 != 0) {
+        countOfEliminatedPlayers = countOfEliminatedPlayers + 1;
+      }
+      if (speedOfPlayer3 != 0) {
+        countOfEliminatedPlayers = countOfEliminatedPlayers + 1;
+      }
+    }
+
+    System.out.println(countOfEliminatedPlayers);
+  }
+}
